8c234f6fd06fba822ff6ecb00ac06b2391c337c4
4R9ES <130325394+4R9ES@users.noreply.github.com>
2023-12-08 11:00:14 +0900

    Update main_project.py
    new feature
    1. displaying time
    2. put an advice with time that the advice was created in advice.txt
    3. give a chance of denying putting an advice in advice.txt

--- main_project.py
+++ main_project.py
@@ -1,5 +1,8 @@
 import requests
+from datetime import datetime
 import json
+
+now = datetime.now()
 
 def fetch_random_advice():
     try:
@@ -13,4 +16,22 @@ def fetch_random_advice():
 
 def save_advice_to_file(advice, filename="advice.txt"):
     with open(filename, "a") as file:
-        file.write(advice + "\n")
+        file.write('[' + now.strftime('%Y-%m-%d %H:%M:%S') + '] ' + advice + ' ' + "\n")
+
+def yes_or_no():
+    print("Would you like to put this advice in advice.txt? ----- Press Y/N")
+    value = input()
+    if value == 'y' or value == 'Y':
+      print("\nSave complete.")
+      return 1
+    else :
+      return 0
+
+random_advice = fetch_random_advice()
+
+if random_advice:
+    print("랜덤 명언:", random_advice, now.strftime('(%Y-%m-%d %H:%M:%S)\n'))
+    if yes_or_no():
+        save_advice_to_file(random_advice)
+else:
+    print("랜덤 명언을 불러오는 데 실패했습니다.")
