483e358078f1a38e94ba94d8c2e27afa73d45468
Musong Lee <anthd911@gmail.com>
2023-12-08 11:08:04 +0900

    branch_2

--- main_project.py
+++ main_project.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import random
 
 def fetch_random_advice():
     try:
@@ -14,3 +15,26 @@ def fetch_random_advice():
 def save_advice_to_file(advice, filename="advice.txt"):
     with open(filename, "a") as file:
         file.write(advice + "\n")
+
+def get_random_advice_from_file(filename="advice.txt"):
+    try:
+        with open(filename, "r") as file:
+            advices = file.readlines()
+            if advices:
+                return random.choice(advices).strip()
+            else:
+                return None
+    except Exception as e:
+        print(f"파일 읽기 중 오류 발생: {e}")
+        return None
+
+if __name__ == "__main__":
+    # 실행할 때마다 랜덤 명언 출력
+    random_advice = fetch_random_advice()
+    if random_advice:
+        print("Random Advice from API:", random_advice)
+        save_advice_to_file(random_advice)
+
+    saved_advice = get_random_advice_from_file()
+    if saved_advice:
+        print("Random Advice from File:", saved_advice)
